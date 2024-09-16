54ac64658168669b8b620a25ce217987ab19ce44
Hawwad <hawwad@Hawwads-MacBook-Pro.local>
2024-09-16 14:56:06 +0200

    fix color converting issue

--- src/threeXbabylon/dictionary/mapper/mesh/babylonMesh.ts
+++ src/threeXbabylon/dictionary/mapper/mesh/babylonMesh.ts
@@ -17,26 +17,46 @@ export class BabylonBox {
   constructor(threeMesh: THREE.Mesh, scene: BABYLON.Scene) {
     BabylonBox._boxNumber++;
     this.name += BabylonBox._boxNumber;
-    this.box = this.convertToBabylonMesh(threeMesh, this.name);
     this.material = new BABYLON.StandardMaterial("boxMaterial", scene);
-    this.box.setMaterialById("boxMaterial");
+    // Get the color from the Three.js mesh material
+    if (
+      threeMesh.material instanceof THREE.MeshBasicMaterial ||
+      threeMesh.material instanceof THREE.MeshStandardMaterial
+    ) {
+      const color = threeMesh.material.color;
+      this.material.diffuseColor = new BABYLON.Color3(
+        color.r,
+        color.g,
+        color.b
+      );
+      if (threeMesh.material.wireframe === true) {
+        this.material.wireframe = true; // Enable wireframe in Babylon.js
+      }
+    }
+    this.box = this.convertToBabylonMesh(threeMesh, this.name);
+    this.box.material = this.material;
+    // Add a Hemispheric light to illuminate the scene (if there's no light in the scene, otherwise skip this step)
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+    light.intensity = 1; // Adjust the intensity as needed
   }
-
 
   /**
    ** Get the Babylon.js mesh
-   * @returns 
+   * @returns
    */
   getBox(): BABYLON.Mesh {
     return this.box;
   }
 
-
   /**
    ** Converts Three.js mesh to Babylon.js mesh
-   * @param threeMesh 
-   * @param name 
-   * @returns 
+   * @param threeMesh
+   * @param name
+   * @returns
    */
   convertToBabylonMesh(threeMesh: THREE.Mesh, name: string): BABYLON.Mesh {
     if (threeMesh.geometry instanceof THREE.BoxGeometry) {
